9f2dc87187b4a8d2603e179eef3cfd454268ea40
Jack Fox <0xdeadbeef1@gmail.com>
2020-12-26 16:03:39 -0500

    Removed debug logs/warns and made code more consistent

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const { getModule, FluxDispatcher } = require('powercord/webpack')
 
 class PopoutFix extends Plugin {
     onPopoutOpen(e) {
-        const window = this.windowStore.getWindow(e.key)
-        this.popouts.set(e.key, window)
-        console.warn("registered", e.key, window)
-        console.log("popouts", window.popouts)
+        const window = this.windowStore.getWindow(e.key);
+        this.popouts.set(e.key, window);
     }
 
     onPopoutClose(e) {
-        this.popouts.delete(e.key)
-        console.warn("unregistered", e.key)
+        this.popouts.delete(e.key);
     }
 
     startPlugin() {
@@ -19,15 +16,13 @@ class PopoutFix extends Plugin {
         this.onPopoutOpen = this.onPopoutOpen.bind(this);
         this.onPopoutClose = this.onPopoutClose.bind(this);
         this.popouts = window.popouts || (function() { throw 'No window popouts' })();
-        console.warn("popouts", this.popouts);
-        console.warn("windowStore", this.windowStore);
-        FluxDispatcher.subscribe('POPOUT_WINDOW_OPEN', this.onPopoutOpen)
-        FluxDispatcher.subscribe('POPOUT_WINDOW_CLOSE', this.onPopoutClose)
+        FluxDispatcher.subscribe('POPOUT_WINDOW_OPEN', this.onPopoutOpen);
+        FluxDispatcher.subscribe('POPOUT_WINDOW_CLOSE', this.onPopoutClose);
     }
 
     pluginWillUnload() {
-        FluxDispatcher.unsubscribe('POPOUT_WINDOW_OPEN', this.onPopoutOpen)
-        FluxDispatcher.unsubscribe('POPOUT_WINDOW_CLOSE', this.onPopoutClose)
+        FluxDispatcher.unsubscribe('POPOUT_WINDOW_OPEN', this.onPopoutOpen);
+        FluxDispatcher.unsubscribe('POPOUT_WINDOW_CLOSE', this.onPopoutClose);
     }
 }
 
